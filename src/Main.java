128c6f0ffcd9064c622ece407fcee2906efc33e5
Matthew Navy <128771121+Saratarr@users.noreply.github.com>
2024-09-05 12:49:11 -0500

    init

---
+++ src/Main.java
@@ -0,0 +1,33 @@
+public class Main {
+
+    public void bubbleArray(int[] arr) {
+        int n = arr.length;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    // swap temp and arr[i]
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    public void printArray(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        Main sort = new Main();
+        int[] arr = {23, 42, 11, 55, 37, 64, 6};
+        sort.bubbleArray(arr);
+        System.out.println("Bubble sorted array: ");
+        sort.printArray(arr);
+    }
+}
